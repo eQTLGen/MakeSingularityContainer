f48bcf57da6b2aecd1cd6ec39287f58082f6c511
Urmo Võsa <urmo.vosa@gmail.com>
2023-03-17 11:28:06 +0200

    Remove memory profiler used for testing and debugging

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 ### Build the Docker container
-sudo docker build -t quay.io/eqtlgen/eqtlgen_phase2:v1.9 .
+sudo docker build -t quay.io/eqtlgen/eqtlgen_phase2:v1.8 .
 
 ### Push to quay.io
-#sudo docker push quay.io/eqtlgen/eqtlgen_phase2:v1.9
+#sudo docker push quay.io/eqtlgen/eqtlgen_phase2:v1.8
